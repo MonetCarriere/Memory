21163d69ff53b4734cce246813be1c01f90a0bdf
Owner <monetcarriere@yahoo.com>
2019-07-23 08:06:16 -0700

    memory final

--- main.js
+++ main.js
@@ -1,5 +1,111 @@
+$(document).ready(function(){
+    var first_card = null;
+    var second_card = null;
+    $(document).on('click','.card .card-face',function(){
+        let matched = $(this).parent().hasClass('matched');
+        let flipped_count = $('.card.flipped').length;
+        if(matched) return false;
+        let flipped = $(this).parent().hasClass('flipped');
+        if(flipped){
 
-/*const images = [
+        }else{
+            $(this).parent().addClass('flipped');
+            flipped_count = $('.card.flipped').length;
+            if(flipped_count === 1){
+                first_card = $(this);
+            }else if(flipped_count == 3){
+            }else if(flipped_count === 2){
+                second_card = $(this);
+                //console.log(first_card);
+                let first_card_style = first_card.parent().children('.card-front').attr('style');
+                let last_card_style = second_card.parent().children('.card-front').attr('style');
+                
+                if(first_card_style == last_card_style){
+                    setTimeout(function(){
+                        first_card.parent().removeClass('flipped').addClass('matched');
+                        second_card.parent().removeClass('flipped').addClass('matched');
+                    },1500);
+                }else{
+                    setTimeout(function(){
+                        $('.flipped').removeClass('flipped');
+                        //second_card.parent().removeClass('flipped');
+                    },1500);
+                }
+            }else{
+
+            }
+        }
+        //$(this).parent().toggleClass('flipped');
+    });
+
+    $(document).on('click','#start_game ',function(){
+        $('.game-container').html('');
+        let images2 = images;
+        images2 = images2.concat(images);
+        shuffle(images2);
+        $.each(images2, function( index, image ) {
+            //console.log(image);
+          let current_image = ` <div class="card"><div class="card-back card-face">
+
+                 </div>
+                <div class="card-front card-face" style="background-image:url(./`+image.image+`);">
+                
+                </div>
+            </div>`;
+            $('.game-container').append(current_image);
+        });
+    });
+});
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+    [array[i], array[j]] = [array[j], array[i]]; // swap elements
+  }
+}
+//Math.floor(Math.random() * 10);
+
+//if(.flipped == .card-front card-face)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const images = [
             {
                 id:1,
                 image:"rob.png"
@@ -36,8 +142,13 @@
             {
                 id:8,
                 image:"kanye.png"
-            }];
-console.log(images);
+            },
+            {
+                id:9,
+                image:"fry.png"
+            }
+            ];
+/*console.log(images);
 
 
 
@@ -66,4 +177,4 @@ function flipCard() {
     }
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard)) 
+cards.forEach(card => card.addEventListener('click', flipCard)) */
